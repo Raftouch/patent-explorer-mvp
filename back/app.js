e46a3c04018d9ae5a079717a9b21b091f019aea6
raftouch <rafanelkin@gmail.com>
2025-12-05 20:00:48 +0100

    server is up and running

--- back/app.js
+++ back/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.get("/", (req, res) => {
+  res.send("Hello ");
+});
+
 const start = async () => {
   try {
     app.listen(port, () => console.log(`App is running on port ${port}`));
