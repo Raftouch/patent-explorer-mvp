884bea85732effb4af6a9e61c554310775af9fb1
raftouch <rafanelkin@gmail.com>
2025-12-07 15:20:17 +0100

    add note fetch, test with fixed data - ok

--- back/app.js
+++ back/app.js
@@ -8,6 +8,19 @@ app.get("/", (req, res) => {
   res.send("Hello ");
 });
 
+app.get("/api/books", async (req, res) => {
+  try {
+    const query = "the lord of the rings";
+    const url = `https://openlibrary.org/search.json?q=${query}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data : ", data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occured while fetching books");
+  }
+});
+
 const start = async () => {
   try {
     app.listen(port, () => console.log(`App is running on port ${port}`));
